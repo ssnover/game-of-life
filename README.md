972c6a62dd793130c824a8b7add36014fccefa01
ssnover <ssnover95@gmail.com>
2022-01-14 19:57:31 -0700

    Busy loop based frame synchronization, drawing a green pixel onto the screen one pixel at a time

---
+++ README.md
@@ -0,0 +1,5 @@
+# game-of-life
+An implementation of a Conway's Game of Life environment on GBA. The ultimate game should have two modes: Edit and Run mode which can be switched between with the Start button (Enter in mGBA). In Edit mode the player can change cells between Alive and Dead. Then in Run mode the player can observe the automata advance according to the rules of Conway's Game of Life.
+
+## Why?
+Why not?
--- src/main.rs
+++ src/main.rs
@@ -1,75 +1,44 @@
 #![no_std]
 #![no_main]
-#![feature(asm, global_asm, isa_attribute)]
 
 use gba::prelude::*;
 
 #[panic_handler]
 #[allow(unused)]
 fn panic(info: &core::panic::PanicInfo) -> ! {
-  loop {
-    DISPCNT.read();
-  }
-}
-
-/// Performs a busy loop until VBlank starts.
-///
-/// This is very inefficient, and please keep following the lessons until we
-/// cover how interrupts work!
-pub fn spin_until_vblank() {
-  while VCOUNT.read() < 160 {}
-}
-
-/// Performs a busy loop until VDraw starts.
-///
-/// This is very inefficient, and please keep following the lessons until we
-/// cover how interrupts work!
-pub fn spin_until_vdraw() {
-  while VCOUNT.read() >= 160 {}
+    loop {
+        DISPCNT.read();
+    }
 }
 
 #[no_mangle]
 pub fn main() -> ! {
-  const SETTING: DisplayControl = DisplayControl::new().with_display_mode(3).with_display_bg2(true);
-  DISPCNT.write(SETTING);
+    const SETTING: DisplayControl = DisplayControl::new()
+        .with_display_mode(3)
+        .with_display_bg2(true);
+    DISPCNT.write(SETTING);
 
-  const RED: Color = Color::from_rgb(31, 0, 0);
-  const GREEN: Color = Color::from_rgb(0, 31, 0);
+    for row in 0..mode3::HEIGHT {
+        for col in 0..mode3::WIDTH {
+            let keys: Keys = KEYINPUT.read().into();
+            //if keys.a() {
+                const GREEN: Color = Color::from_rgb(0, 31, 0);
+                write_pixel(col, row, GREEN);
+            //}
+            spin_cycle();
+        }
+    }
 
-  let mut keys = Keys::new();
+    loop {}
+}
 
-  fn draw_square(x: usize, y: usize, color: Color) {
+fn write_pixel(x: usize, y: usize, color: Color) {
     mode3::bitmap_xy(x, y).write(color);
-    mode3::bitmap_xy(x, y + 1).write(color);
-    mode3::bitmap_xy(x, y + 2).write(color);
-    mode3::bitmap_xy(x + 1, y).write(color);
-    mode3::bitmap_xy(x + 1, y + 1).write(color);
-    mode3::bitmap_xy(x + 1, y + 2).write(color);
-    mode3::bitmap_xy(x + 2, y).write(color);
-    mode3::bitmap_xy(x + 2, y + 1).write(color);
-    mode3::bitmap_xy(x + 2, y + 2).write(color);
-  }
+}
 
-  loop {
-    // wait until we're into the vblank period
-    spin_until_vdraw();
-    spin_until_vblank();
-
-    draw_square(1, 1, if keys.l() { GREEN } else { RED });
-    draw_square(mode3::WIDTH - 4, 1, if keys.r() { GREEN } else { RED });
-    //
-    draw_square(mode3::WIDTH - 4, mode3::HEIGHT / 2, if keys.a() { GREEN } else { RED });
-    draw_square(mode3::WIDTH - 4 - 20, mode3::HEIGHT / 2 + 20, if keys.b() { GREEN } else { RED });
-    //
-    draw_square(mode3::WIDTH / 2 + 10, mode3::HEIGHT - 5, if keys.start() { GREEN } else { RED });
-    draw_square(mode3::WIDTH / 2 - 10, mode3::HEIGHT - 5, if keys.select() { GREEN } else { RED });
-    //
-    draw_square(30, mode3::HEIGHT / 2 - 20, if keys.up() { GREEN } else { RED });
-    draw_square(30, mode3::HEIGHT / 2 + 20, if keys.down() { GREEN } else { RED });
-    draw_square(10, mode3::HEIGHT / 2, if keys.left() { GREEN } else { RED });
-    draw_square(50, mode3::HEIGHT / 2, if keys.right() { GREEN } else { RED });
-
-    // read our keys for next frame
-    keys = KEYINPUT.read().into();
-  }
+fn spin_cycle() {
+    for _ in 0..8 {
+        while VCOUNT.read() < 160 {}
+        while VCOUNT.read() >= 160 {}
+    }
 }
